76dc0c99a9c1166d4ac21dd8022d62a6c5ce1161
Jon DeJong <jon.dejong@gmail.com>
2016-01-26 21:24:18 -0600

    just backing up the things

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,19 @@
 import {Component} from 'angular2/core'
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}}</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 
 export class AppComponent {
-    public title = 'Tour of Heros';
-    public hero = 'Windstorm';
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
 }
