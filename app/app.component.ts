33e8eb47d3acd393da2561d224d27bb35d8f0c05
Jon DeJong <jon.dejong@gmail.com>
2016-01-26 21:13:51 -0600

    just backing up the things

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,10 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello World 2</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero}}</h2>'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Tour of Heros';
+    public hero = 'Windstorm';
+}
